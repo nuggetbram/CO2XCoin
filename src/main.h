84c0130926e2ebc9be05d41634955d5a6bde1ef9
aetherum <admin@aetherum.com>
2015-02-11 19:57:16 +1100

    .o

--- src/main.h
+++ src/main.h
@@ -58,7 +58,7 @@ inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONE
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
-#define FOUNDATION_ADDRESS "GLAHVwoTMJzvTsFyQn1nHArYK2zYwi4fF7"
+#define FOUNDATION_ADDRESS "GaCiQQNirDBqhE3c6n8QaPwTjXTvAWs39X"
 #define FOUNDATION_ADDRESS_TEST "mwmPTAA7cSDY8Dd5rRHuYitwS2hByXQpdA"
 
 #ifdef USE_UPNP
